e2591cf3532ea860dc4a3b57a1bf25e345b58309
kangchenghua <chenghuakang@gmail.com>
2026-05-08 23:26:29 +0200

    Update example.py

--- example.py
+++ example.py
@@ -81,8 +81,10 @@ if __name__ == "__main__":
     # sn.run_routing_daemon()
     
     # run OSPF daemon on selected nodes
-    sn.run_routing_daemon(node_lst=[
-      'GS0', 'SH1O25S14', 'SH1O26S14', 'SH1O27S14', 'SH1O27S13', 'GS1'])
+
+    sn.run_routing_daemon(node_lst=sn.get_node_names())
+    #sn.run_routing_daemon(node_lst=[
+     # 'GS0', 'SH1O25S14', 'SH1O26S14', 'SH1O27S14', 'SH1O27S13', 'GS1'])
 
     sn.check_routing_table('GS0', 4)
 
